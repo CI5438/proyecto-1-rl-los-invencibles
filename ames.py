9f2985209f4b513eb031f9e3b1f637ad73903421
David Cabeza <cabezadavide@gmail.com>
2018-05-28 15:31:41 -0400

    Añade la funcionalidad de calcular dummies

--- ames.py
+++ ames.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import sys
 
-def read_file(filename):
-    """Reads file and process it using panda.
+def dummies(df):
+    """Get dummies variables for categorical data
     
-    :param: filename Name of the file
+    :param: dataframe
+    :return: dataframe
     """
+    return pd.get_dummies(df)
+
+def read_file(filename):
+    """Reads file and process it using panda dataframes.
+    
+    :param: name of the file
+    :return: dataframe
+    """
+
     try:
         df = pd.read_csv(filename)
         return df
@@ -15,6 +25,7 @@ def read_file(filename):
 
 def init():
     df = read_file("ww2.amstat.org.txt")
+    df = dummies(df)
 
 if __name__ == '__main__':
     init()
