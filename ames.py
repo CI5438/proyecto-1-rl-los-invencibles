9a5627966234e2ea8ea6bbd1d07159a57951bde5
David Cabeza <cabezadavide@gmail.com>
2018-05-28 20:37:29 -0400

    Add row deletion by categorical column value and entire column deletion

--- ames.py
+++ ames.py
@@ -1,8 +1,26 @@
 import pandas as pd
 import sys
 
+def drop_column(df, column):
+    """Drop from dataframe 'column'
+    """
+    try:
+        df = df.drop(column, axis=1)
+    except ValueError as err:
+        print(err)
+
+    return df
+
+def only_rows_from_categorical_column_value(df, column, value):
+    """Remove rows from dataframe.
+
+    Remaining dataframe will have ONLY rows whose column 'column' 
+    has value 'value'
+    """
+    return df.loc[df[column] == value]
+
 def dummies(df):
-    """Get dummies variables for categorical data
+    """Get dummies variables for categorical data.
     
     :param: dataframe
     :return: dataframe
@@ -20,11 +38,22 @@ def read_file(filename):
         df = pd.read_csv(filename)
         return df
     except:
-        print("Error al leer el archivo")
+        print("File couldn't be read")
         sys.exit(-1)
 
 def init():
     df = read_file("ww2.amstat.org.txt")
+    
+    # a) Data cleaning
+    df = only_rows_from_categorical_column_value(df, "Sale Condition", "Normal")
+    df = drop_column(df, 'PID')
+    df = drop_column(df, 'Order')
+
+    # b) Normalization of data
+    
+    # c) Data splitting
+    # df_training, df_validation = get_samples(df)
+
     df = dummies(df)
 
 if __name__ == '__main__':
